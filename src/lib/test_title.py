d5917d52eb0ed7c66c81128548927886768cb388
Göran Gustafsson <gustafsson.g@gmail.com>
2022-09-08 06:18:03 +0200

    Added title, lib/title.py & lib/test_title.py

---
+++ src/lib/test_title.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# pylint: disable=protected-access
+
+import unittest
+
+import title
+
+
+class TestTitle(unittest.TestCase):
+    def test_title(self):
+        """Validate title.title() results."""
+
+        # Test if endings with "'s" and chars "åäö" are handled correctly.
+        string = "göran's top secret quote & å ä ö test..."
+        expect = "Göran's Top Secret Quote & Å Ä Ö Test..."
+        #          ^^  ^^                    ^ ^ ^
+        self.assertEqual(title.titleize(string), expect)
+
+    def test_titleize(self):
+        """Validate title.titleize() results."""
+
+        # Test if all words in _lowercase list gets lowered.
+        lower = str.join(" ", title._lowercase)
+        string = f"first {lower} last"
+        expect = f"First {lower} Last"
+        self.assertEqual(title.titleize(string), expect)
+
+        # Test if all chars in _end_sentence list triggers new sentence.
+        for char in title._end_sentence:
+            string = f"first {char} the last"
+            expect = f"First {char} The Last"
+            self.assertEqual(title.titleize(string), expect)
+
+        # Test if _lowercase list is ignored following bracket or quote.
+        sidestep_chars = ("(", "{", "[", "<", '"', "'")
+        for char in sidestep_chars:
+            string = f"first {char}the last"
+            expect = f"First {char}The Last"
+            self.assertEqual(title.titleize(string), expect)
+
+        # Song/album test 1.
+        string = "anna von hausswolff - the truth, the glow, the fall"
+        expect = "Anna von Hausswolff - The Truth, The Glow, The Fall"
+        #              ^^^            ^^^^^      ^^^^^     ^^^^^
+        self.assertEqual(title.titleize(string), expect)
+
+        # Song/album test 2.
+        string = "bob marley & the wailers - no woman, no cry (live)"
+        expect = "Bob Marley & The Wailers - No Woman, No Cry (Live)"
+        #                    ^^^^^                            ^^^^^
+        self.assertEqual(title.titleize(string), expect)
+
+        # Song/album test 3.
+        string = "danzig - satan (from satan's sadists)"
+        expect = "Danzig - Satan (From Satan's Sadists)"
+        #                        ^^^^^      ^^
+        self.assertEqual(title.titleize(string), expect)
+
+        # Song/album test 4.
+        string = "tears for fears @ rule the world: the greatest hits"
+        expect = "Tears for Fears @ Rule the World: The Greatest Hits"
+        #               ^^^              ^^^      ^^^^^
+        self.assertEqual(title.titleize(string), expect)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/lib/title.py
@@ -0,0 +1,89 @@
+"""Title capitalization module.
+
+Contains functions used for reformatting input text following English title
+capitalization rules. First word, last word, new sentence, brackets () {} []
+<>, quotes "" '', etc is handled correctly. Corner case proofness is good but
+should not be expected.
+
+Lowercase:
+
+    a, an, and, as, at, but, by, en, etc, for, from,
+    if, in, of, on, or, the, to, via, von, vs, with
+
+Example:
+
+    Input:  "tears for fears @ rule the world: the greatest hits"
+    Output: "Tears for Fears @ Rule the World: The Greatest Hits"
+
+Usage:
+
+    title.titelize(string)
+
+Author: Göran Gustafsson <gustafsson.g@gmail.com>
+License: BSD 3-Clause
+"""
+
+import re
+
+_lowercase = (
+    "a", "an", "and", "as", "at", "but", "by", "en", "etc", "for", "from",
+    "if", "in", "of", "on", "or", "the", "to", "via", "von", "vs", "with"
+)
+_end_sentence = (".", ",", ":", ";", "!", "?", "&", "/", "+", "-")
+
+
+def title(text: str) -> str:
+    """Capitalize every word (including sub-words) in string.
+
+    Same as builtin str.title() but it actually works.
+    See: https://docs.python.org/3/library/stdtypes.html#str.title
+
+    Arguments:
+        text: str containing text to titlecase.
+
+    Returns:
+        str containing titlecased text.
+    """
+
+    return re.sub(
+        r"\w+('\w+)?",
+        lambda match: match.group(0).capitalize(),
+        text
+    )
+
+
+def titleize(text: str) -> str:
+    """Capitalize string following English title capitalization rules.
+
+    Arguments:
+        text: str containing text to titleize.
+
+    Returns:
+        str containing titleized text.
+    """
+
+    words = text.split()
+    last_word = len(words) - 1
+    skip_next = False
+
+    # Appending to new list is more efficient than updating existing list.
+    result = []
+
+    for index, word in enumerate(words):
+        # Check if current iteration should be treated as new sentence.
+        skip = bool(skip_next)
+
+        # Check if next iteration should be treated as new sentence.
+        skip_next = bool(word[-1] in _end_sentence)
+
+        # 1. Titlecase first and last word.
+        # 2. Lowercase word unless new sentence.
+        # 3. Titlecase everything else.
+        if index in (0, last_word):
+            result.append(title(word))
+        elif word in _lowercase and not skip:
+            result.append(word.lower())
+        else:
+            result.append(title(word))
+
+    return str.join(" ", result)
