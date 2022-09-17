898448876f7fab161db608016e6eb29f56c072e8
Göran Gustafsson <gustafsson.g@gmail.com>
2022-09-17 08:24:30 +0200

    Add lib/pytt.py

---
+++ src/lib/pytt.py
@@ -0,0 +1,45 @@
+"""Pyttipanna DRY module.
+
+Contains miscellaneous functions that are good enough for re-use but small
+enough to not warrant its own module.
+
+Usage:
+
+    from lib.pytt import x
+
+Author: Göran Gustafsson <gustafsson.g@gmail.com>
+License: BSD 3-Clause
+"""
+
+import os
+
+
+# Used in "gitpl" and "gitst".
+def git_find(dir_: str, sub_level: bool = False) -> list:
+    """Find Git repos under directory.
+
+    Checks if ".git" directory exists under sub-directories. Optionally checks
+    one sub-level down.
+
+    Arguments:
+        dir_: os.DirEntry containing directory to search.
+        sub_level: bool that sets if sub-level directories should be searched.
+
+    Returns:
+        list containing Git repo directories.
+    """
+
+    result = []
+    if sub_level:
+        for subdir in os.scandir(dir_):
+            if not os.path.isdir(subdir.path):
+                continue
+
+            for subdir2 in os.scandir(subdir.path):
+                if os.path.isdir(f"{subdir2.path}/.git"):
+                    result.append(subdir2)
+    else:
+        for subdir in os.scandir(dir_):
+            if os.path.isdir(f"{subdir.path}/.git"):
+                result.append(subdir)
+    return result
